78348c2fc37c654a8152575f3f6790a64cb50269
Francha04 <francisco@altomonte.com.ar>
2020-09-19 20:35:58 -0300

    Main Bulk of Algorithm work
    - Created a Scene Container (as a prefab). Will contain the plot card, the set card, and the characters.
    - Created the Algorithm game object with its correspondent script, it'll contain all the telenovela scenes currently in play.
    - Created a Data Holder with the values needed for formulas.
    - Made every value public so they can be quickly tested in editor.
    - Developed a way to test the algorithmby pressings pace on play mode (will print values on console).

---
+++ Assets/Algorithm.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Algorithm : MonoBehaviour
+{
+    // This script only needs to be present and active on ScreenWriting phase, and its values are shown only in the aftermath phase. It's irrelevant in other phases.
+    // It must contain the Scenes of the current phase (so it can call the calculateTotal method for each scene)
+    // We need to define where we'll store the player's money and audience.
+    public  SceneContainer[] currentScenes;
+    public DataStorage dataC; // Short for dataContainer.
+    int TotalAudience;
+    float multiplier;
+
+    public int getTotalAudience() 
+    {
+        multiplier = dataC.episodeBonus[dataC.currentEpisode]; // The base value of the multiplier depends on the current level.
+        TotalAudience = dataC.baseAudience;
+        for (int k = 0; k < currentScenes.Length; k++) 
+        {
+            multiplier = multiplier + currentScenes[k].calculateTotal();
+        }
+        TotalAudience = TotalAudience * (int)multiplier;
+        return TotalAudience;
+    }
+    // The following two functions should only be called after getTotalAudience has been executed at least once. (I can refactor this  if it becomes a problem).
+    public double getRating() 
+    {
+        double rating;            //Maybe this can be an integer for simplicity? if it stays as a double we should only display the first decimal I believe (0.0 format)
+        rating = multiplier / dataC.audienceToRating;
+        return rating;
+
+    }
+    public int getIncome() // I'll set an extremely placeholder formula here so we can get an idea of the numbers. I'll adapt it once we get the final prices for the items.
+    {
+        int income;
+        income = (int)(TotalAudience * dataC.audienceToIncome);
+        return income;
+    }
+}
--- Assets/SceneContainer.cs
+++ Assets/SceneContainer.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class SceneContainer : MonoBehaviour
 {
-    SetCard chosenSet;
-    PlotCard chosenPlot;
-    CharacterCard[] chosenChars;
-    float sceneTotal;
+    // No need for these to be public. This is only for testing.
+    public SetCard chosenSet;
+    public PlotCard chosenPlot;
+    public CharacterCard[] chosenChars;
+    public DataStorage dataContainer;
 
-    void calculateTotal() 
+    public float calculateTotal()
     {
-        CardType[] setTypes = chosenSet.cardTypes;
-        CardType[] character1Type = chosenChars[0].cardTypes;
-        if (chosenPlot.charactersRequired > 1) { CardType[] character2Type = chosenChars[1].cardTypes; }
-        if (chosenPlot.charactersRequired > 2) { CardType[] character2Type = chosenChars[2].cardTypes; }
-
+        float total = 1;
+        for (int i = 0; i < chosenChars.Length; i++)
+        {
+            if (ComparePlot(i)) { total = total * dataContainer.goodBonus; }
+            else { total = total * dataContainer.neutralBonus; }
+        }
+        for (int i = 0; i < chosenChars.Length; i++)
+        {
+            if (CompareSet(i)) { total = total + dataContainer.setBonus; }
+        }
+        if (chosenSet.cardTypes == chosenPlot.cardTypes) { total = total + dataContainer.setBonus  ;  }
+        return total;
+    }
+     bool ComparePlot(int c) 
+    {
+        if (chosenChars[c].cardTypes == chosenPlot.cardTypes)
+        {
+            return true;
+        }
+        else return false;
+    }
+     bool CompareSet (int c)
+    {
+        if (chosenChars[c].cardTypes == chosenSet.cardTypes)
+        {
+            return true;
+        }
+        else return false;
     }
     
 }
