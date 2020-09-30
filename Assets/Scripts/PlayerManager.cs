215fdc35e7d55e952675392fd852036ec3ed5ddd
Francha04 <francisco@altomonte.com.ar>
2020-09-30 14:42:17 -0300

    Progress on scriptwriting
    - Added all cards we're gonna use on the demo.
    - Developed a deck system.
    - Developed the system of a random card generation (plot cards) and adding those cards to the player's hand.

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager instance = null;
+
+
+    public static List<SetCard> setCards = new List<SetCard>();
+    public static List<CharacterCard> characterCards = new List<CharacterCard>();
+    public static List<PlotCard> plotCards = new List<PlotCard>();
+
+    public static float totalMoney = 0f;
+
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+        DontDestroyOnLoad(this.gameObject);
+        AddStarterCards();
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject); //Is this redundant since we have the DontDestroy in the void start?
+    }
+
+    void Update()
+    {
+
+    }
+    public List<PlotCard> playerPlots()
+    {
+        return plotCards;   
+    }
+    void AddStarterCards()
+    {
+        SetCard[] starterSets = Resources.LoadAll<SetCard>("Assets / Prefabs / Cards / Sets / Starter");
+        PlotCard[] starterPlot = Resources.LoadAll<PlotCard>("Assets / Prefabs / Cards / Plot / Starter");
+        CharacterCard[] starterChar = Resources.LoadAll<CharacterCard>("Assets / Prefabs / Cards / Character / Starter");
+
+        for (int i = 0; i < starterSets.Length; i++) 
+        {
+            setCards.Add(starterSets[i]);
+        }
+        for (int i = 0; i < starterPlot.Length; i++)
+        {
+            plotCards.Add(starterPlot[i]);
+        }
+        for (int i = 0; i < starterChar.Length; i++)
+        {
+            characterCards.Add(starterChar[i]);
+        }
+    }
+}
---
+++ Assets/Scripts/deckManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using UnityEngine;
+
+public class deckManager : MonoBehaviour
+{
+    public PlayerManager player;
+    public PlotCard[] deck;
+    public int playerHand = 5; // We can dinamically change this in runtime, but for the demo it's fine to just set it at 5 manually.
+    void Start()
+    {
+        LoadAllPlots();
+        if (player.playerPlots().Count < playerHand + 1)
+        {
+            int boughtCards = player.playerPlots().Count;
+            GenerateRandomCards(boughtCards);
+        }
+    }
+
+    void LoadAllPlots()
+    {
+        PlotCard[] notStarterDeck;
+        notStarterDeck = Resources.LoadAll<PlotCard>("Assets/Prefabs/Cards/Plots");
+        int totalNotStarterCards = notStarterDeck.Length;
+        PlotCard[] starterDeck;
+        starterDeck = Resources.LoadAll<PlotCard>("Assets / Prefabs / Cards / Plots / Starter");
+        int totalStarterCards = starterDeck.Length;
+        for (int i = totalNotStarterCards; i < totalNotStarterCards + totalStarterCards; i++)
+        {
+            notStarterDeck[i] = starterDeck[i - totalNotStarterCards];
+        }
+        deck = notStarterDeck;
+    }
+
+    public void GenerateRandomCards(int x)
+    {
+        PlotCard[] newHand = new PlotCard[playerHand];
+        for (int y = x; y <= x; y++) 
+        {
+            newHand[y - x] = player.playerPlots().ElementAt<PlotCard>(y - x);
+        }
+        for (int t = x; t <= playerHand; t++) 
+        {
+            
+        }
+    }
+    
+}
